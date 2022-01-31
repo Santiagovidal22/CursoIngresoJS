9e82b4a19cb2b44497810e60d3f47722d6b4bc24
Santiagovidal22 <98592373+Santiagovidal22@users.noreply.github.com>
2022-01-31 12:27:53 -0300

    ejercicios del 1 al 5

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,29 @@
-/*
+/*Santiago Vidal DIV E 
+E/S ej2
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	alert("ok");
+{   
+//declaro variable
+var nombreIngresado
+
+//guardo nombre en la variable 
+nombreIngresado=prompt("Ingrese su nombre");
+
+
+//muestro el nombre 
+	alert("su nombre es " + nombreIngresado);
 }
 
+
+/*
+
+declaro variable
+
+entrada 
+-prompt
+
+salida
+-alert
+
+*/
